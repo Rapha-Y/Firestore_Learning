02b70caa9a04cc64b47c30cc4e4c19960e947397
Rapha-Y <raph.yuki@gmail.com>
2020-04-19 19:21:16 -0300

    added data fetching

---
+++ app.js
@@ -0,0 +1,22 @@
+const jobList = document.querySelector("#job-list");
+
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute("data-id", doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    jobList.appendChild(li);
+}
+
+db.collection("jobs").get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderCafe(doc);
+    });
+})
